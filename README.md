4772fda166c67ab0576903d948fc8c9e123f0265
Jeremy Rimpo <jeremy.rimpo@servermonkey.com>
2023-05-01 14:49:33 -0500

    Update copyrights and readme

--- README.md
+++ README.md
@@ -2,12 +2,14 @@
 
 This is a simple [Elite Dangerous Market Connector][EDMC] plugin to add a handy display for your current NavRoute.
 
-Once a route has been plotted, the display will indicate the remaining jumps as well as the next and final system on your route.
+Once a route has been plotted, the display will indicate the remaining jumps as well as a configurable number of
+interim jumps between your current position and final target.
 
-If you jump to a system that is not on your route, the plugin will indicate this and suggest the next unvisited system on the route.
+If you jump to a system that is not on your route, the plugin will indicate this and suggest the next unvisited
+system on the route.
 
 ## Requirements
-* EDMC version 5 and above
+* EDMC version 5.7 and above
 
 ## Installation
 * Download the [latest release]
@@ -17,7 +19,7 @@ If you jump to a system that is not on your route, the plugin will indicate this
 
 ## License
 
-[NavRoute plugin][NavRoute] Copyright © 2022 Jeremy Rimpo
+[NavRoute plugin][NavRoute] Copyright © 2023 Jeremy Rimpo
 
 Licensed under the [GNU Public License (GPL)][GPLv2] version 2 or later.
 
--- load.py
+++ load.py
@@ -1,26 +1,10 @@
 # -*- coding: utf-8 -*-
+# NavRoute plugin for EDMC
+# Source: https://github.com/Silarn/EDMC-NavRoute
 #
-# Copyright (C) 2021 Jeremy Rimpo
-# Copyright (C) 2017 Jonathan Harris
-# 
-# This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation; either version 2
-# of the License, or (at your option) any later version.
-# 
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-# 
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-#
-# -----------------------------------------------------------
-#
-# Display the current navigation route
-#
+# Copyright (C) 2023 Jeremy Rimpo
+# Licensed under the [GNU Public License (GPL)](http://www.gnu.org/licenses/gpl-2.0.html) version 2 or later.
+
 import tkinter as tk
 from tkinter import ttk
 
@@ -32,7 +16,6 @@ from theme import theme
 from typing import Any, MutableMapping, Mapping
 from EDMCLogging import get_main_logger
 from ttkHyperlinkLabel import HyperlinkLabel
-
 
 class This:
     """Holds module globals."""
